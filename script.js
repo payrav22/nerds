1297c2898f53f180dc1fdca51bed762f2bd189d3
payrav22 <payravisoev22@gmail.com>
2023-03-06 16:24:55 +0500

    module 9

---
+++ script.js
@@ -0,0 +1,18 @@
+let popup = document.querySelector('.modal');
+let openPopupButton = document.querySelector('.button-open');
+let closePopupButton = popup.querySelector('.button-close');
+
+openPopupButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popup.classList.add('modal--show');
+});
+
+closePopupButton.addEventListener('click', function () {
+  popup.classList.remove('modal--show');
+});
+
+document.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    popup.classList.remove('modal--show');
+  }
+});
